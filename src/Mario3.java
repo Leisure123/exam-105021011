e741988454addbae3baca60798d0b0f4e5b9017e
Leisure123 <105021011@live.asia.edu.tw>
2018-01-13 17:20:05 +0800

    20180113_2

---
+++ src/Mario3.java
@@ -0,0 +1,252 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Mario3 implements Runnable{
+    private ImageStore store = new ImageStore();
+    private Scenes scenes = new Scenes();
+
+    private int x , y;
+    private int movex = 0, movey = 0;
+    private int upTime;
+    //jump = true; ground = false;
+    private boolean upCheck = false;
+    // Right = true; Left = false
+    private boolean RLCheck = true;
+    private int life = 0;
+
+    private BufferedImage nowImage;
+    private BufferedImage lifeImage;
+    private String status;
+    private Thread t = null;
+
+    public Mario3(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.nowImage = store.marioImage.get(1);
+        this.lifeImage = store.headlife.get(0);
+        this.status = "right";
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void rightMove(){
+        movex = 5;
+        RLCheck = true;
+        if (!upCheck){
+            status = "right";
+        }else{
+            status = "jump-right";
+        }
+    }
+
+    public void leftMove(){
+        movex = -5;
+        RLCheck = false;
+        if (!upCheck){
+            status = "left";
+        }else{
+            status = "jump-left";
+        }
+    }
+
+    public void rightStop(){
+        this.movex = 0;
+        RLCheck = true;
+        if (!upCheck){
+            status = "right";
+        }else{
+            status = "jump-right";
+        }
+    }
+
+    public void leftStop(){
+        this.movex = 0;
+        RLCheck = false;
+        if (!upCheck){
+            status = "left";
+        }else{
+            status = "jump-left";
+        }
+    }
+
+    public void jump(){
+        // 限制跳一次
+        if (!upCheck){
+            if(RLCheck){
+                status = "jump-right";
+            }else{
+                status = "jump-left";
+            }
+            movey = -5;
+            upTime = 32;
+            upCheck = true;
+        }
+    }
+
+    public void drop(){
+        if(RLCheck){
+            status = "jump-right";
+        }else{
+            status = "jump-left";
+        }
+        movey = 5;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            boolean canL = true, canR = true, onLand = false;
+            //miniMario
+            for(int i = 0; i < scenes.getBricks2().size(); i++){
+                Brick br = scenes.getBricks2().get(i);
+                //不允許往右
+                if((this.x + 50 == br.getX()) && (this.y < br.getY() + 50 && this.y > br.getY() - 50)){
+                    canR = false;
+                }
+                //不允許往左
+                if((this.x - 50 == br.getX()) && (this.y < br.getY() + 50 && this.y > br.getY() - 50)){
+                    canL = false;
+                }
+                //可以站在障礙物上
+                if((this.y + 50 == br.getY()) && (this.x < br.getX() + 50 && this.x > br.getX() - 50)){
+                    if (br.getType() != 5 && br.getType() != 6 && br.getType() != 7){
+                        onLand = true;
+                    }
+                }
+                //撞到障礙物
+                if((this.y - 50 == br.getY()) && (this.x < br.getX() + 50 && this.x > br.getX() - 50)){
+//                    if (br.getType() == 4){
+//                        br.setType(1);
+//                        br.setImage();
+//                    }
+//                    if(br.getType() == 1){
+//                        scenes.getBricks().remove(br);
+//                        scenes.getBrickRe().add(br);
+//                    }
+//                    if(br.getType() == 2 && upTime > 0){
+//                        br.setType(3);
+//                        br.setImage();
+//                    }
+                    upTime = 0;
+                }
+            }
+
+            if (((this.x + 50 == 1460) && (this.y < 550 && this.y > 340))){
+                JOptionPane.showMessageDialog(null,"Ending");
+                System.exit(0);
+
+            }
+
+            if(onLand && upTime ==0){
+                if(RLCheck){
+                    status = "right";
+                }else{
+                    status = "left";
+                }
+                upCheck = false;
+            }else{
+                if(upTime != 0){
+                    upTime--;
+                }else{
+                    this.drop();
+                }
+                y += movey;
+            }
+
+            if(this.y > 600){
+                dead();
+            }
+
+            if((canL && (movex < 0)) || (canR && (movex > 0))){
+                if(x < 0 || x > 1500){
+                    x = 0;
+                }
+                x += movex;
+            }
+
+//            this.nowImage = store.marioImage.get(0);
+
+            switch (status){
+                case "right":
+                    this.nowImage = store.marioImage.get(0);
+                    break;
+                case "left":
+                    this.nowImage = store.marioImage.get(1);
+                    break;
+                case "jump-right":
+                    this.nowImage = store.marioImage.get(2);
+                    break;
+                case "jump-left":
+                    this.nowImage = store.marioImage.get(3);
+                    break;
+            }
+
+            switch (life){
+                case 0:
+                    this.lifeImage = store.headlife.get(0);
+                    break;
+                case 1:
+                    this.lifeImage = store.headlife.get(1);
+                    break;
+                case 2:
+                    this.lifeImage = store.headlife.get(2);
+                    break;
+            }
+
+            try{
+                Thread.sleep(10);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public int getMovex(){
+        return movex;
+    }
+
+    public int getMovey(){
+        return movey;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public BufferedImage getNowImage(){
+        return nowImage;
+    }
+
+    public BufferedImage getLifeImage(){
+        return lifeImage;
+    }
+
+    public void dead(){
+        this.x = 5;
+        this.y = 100;
+        life++;
+        if (life == 3){
+
+            JOptionPane.showMessageDialog(null,"You are Dead");
+            System.exit(0);
+
+        }
+    }
+
+
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+}
---
+++ src/Test4.java
@@ -0,0 +1,139 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+public class Test4 extends JFrame implements Runnable{
+    private int width = 1500,height = 600;
+    private int scrW = Toolkit.getDefaultToolkit().getScreenSize().width/2;
+    private int scrH = Toolkit.getDefaultToolkit().getScreenSize().height/2;
+    //    String imagePath = System.getProperty("user.dir") + "/Image/";
+    private BufferedImage image;
+    private Graphics g2;
+    private int time = 0;
+    private Timer timer;
+
+    private ImageStore store = new ImageStore();
+    private Scenes scenes = new Scenes();
+
+    private Thread t = new Thread(this);
+    private boolean check = false;
+
+    private Mario mario = null;
+
+    public Test4(){
+        this.setBounds(scrW-(width/2),scrH-(height/2),width,height);
+        this.setResizable(false);
+        this.setTitle("Mario_試煉之地");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        this.mario = new Mario(5,480);
+
+        this.repaint();
+
+        t.start();
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                timer.start();
+            }
+        });
+
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_LEFT:
+                        mario.leftMove();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        mario.rightMove();
+                        break;
+                    case KeyEvent.VK_UP:
+                        mario.jump();
+                        break;
+                    case KeyEvent.VK_SPACE:
+                        mario.jump();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_LEFT:
+                        mario.leftStop();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        mario.rightStop();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                time++;
+            }
+        });
+
+    }
+
+
+    public void paint(Graphics g){
+        BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR);
+        Graphics g2 = image.getGraphics();
+        g2.drawRect(0,0,width,height);
+        //畫背景
+        g2.drawImage(store.backGround,0,0,this);
+        g2.drawImage(store.endingflag,1450,320,this);
+        g2.setFont(new Font(null,Font.BOLD,25));
+        //paint Timer
+        g2.drawString("Time",1370,60);
+        g2.drawString(time+"s",1370,90);
+        //paint headlife
+        g2.drawString("Life : ",20,65);
+        g2.drawImage(mario.getLifeImage(),80,45,this);
+        //畫出障礙物
+        for(int i = 0;i < scenes.getBricks().size(); i++){
+            Brick br = scenes.getBricks().get(i);
+            g2.drawImage(br.getShowImage(),br.getX(),br.getY(),this);
+        }
+        //畫Mario
+        g2.drawImage(mario.getNowImage(),this.mario.getX(),this.mario.getY(),this);
+
+        g.drawImage(image,0,0,this);
+
+    }
+
+    public void run(){
+        while(true){
+            this.repaint();
+            if (((mario.getX() + 50 == 1460) && (mario.getY() < 380 && mario.getY() > 280)) || ((mario.getY() + 50 == 330) && (mario.getX() < 1500 && mario.getX() > 1410))){
+//                System.exit(1);
+//                Test3 t3 = new Test3(Test2.this);
+//                t3.setVisible(true);
+//                Test2.this.dispose();
+            }
+            try{
+                Thread.sleep(10);
+//                System.out.println(imagePath);
+            }catch(Exception q){
+                System.out.println(q);
+            }
+        }
+
+    }
+
+}
